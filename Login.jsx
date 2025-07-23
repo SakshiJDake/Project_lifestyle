9f5cafb32aa5b11214f785a25bb5373fc70320e1
admin <Sakshi Dake>
2025-07-23 12:13:36 +0530

    registration for admin

---
+++ Login.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { useNavigate} from 'react-router-dom';
+
+const Login = () => {
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) =>
+    {
+        e.preventDefault();
+
+        navigate('/');
+    }
+  return (
+    <div className="flex flex-col items-center justify-center h-screen dark">
+      <div className="w-full max-w-md bg-gray-800 rounded-lg shadow-md p-6">
+        <h2 className="text-2xl font-bold text-gray-200 mb-4">Login </h2>
+        <form className="flex flex-col">
+          <div className="flex space-x-4 mb-4">
+            <input
+              placeholder="First Name"
+              className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 w-1/2 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+              type="text"
+            />
+            <input
+              placeholder="Last Name"
+              className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 w-1/2 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+              type="text"
+            />
+          </div>
+          <input
+            placeholder="Email"
+            className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+            type="email"
+          />
+          <input
+            placeholder="Confirm Email"
+            className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+            type="email"
+          />
+          <input
+            placeholder="Password"
+            className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+            type="password"
+          />
+          <input
+            placeholder="Confirm Password"
+            className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+            type="password"
+          />
+          <label className="text-sm mb-2 text-gray-200 cursor-pointer" htmlFor="gender">
+            Gender
+          </label>
+          <select
+            className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+            id="gender"
+          >
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+          <label className="text-sm mb-2 text-gray-200 cursor-pointer" htmlFor="age">
+            Age
+          </label>
+          <input
+            className="bg-gray-700 text-gray-200 border-0 rounded-md p-2"
+            id="age"
+            type="date"
+          />
+          <p className="text-white mt-4">
+            Already have an account?
+            <a className="text-sm text-blue-500 hover:underline ml-1" href="#">
+              Login
+            </a>
+          </p>
+          <button
+            className="bg-gradient-to-r from-skin-500 to-brown-400 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150"
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
